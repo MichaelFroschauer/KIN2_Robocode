df2cdafe06b619c4ca410cfe4d7573d1dbf86040
Michael Froschauer <mi.froschauer@gmx.at>
2025-04-02 22:28:59 +0200

    Upgrade NoobSlayer500 to NoobSlayer1000 by implementing ML-powered gun system

--- robots/src/kin/GunV4_GFSurfer_Merge_Shortend.java
+++ robots/src/kin/GunV4_GFSurfer_Merge_Shortend.java
@@ -4,8 +4,21 @@ import robocode.*;
 import robocode.util.Utils;
 
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.util.*;
 
-public class GunV4_GFSurfer_Merge extends AdvancedRobot {
+public class GunV4_GFSurfer_Merge_Shortend extends AdvancedRobot {
+
+  private static final double BULLET_POWER = 1.9;
+  private static double lateralDirection;
+  private static double lastEnemyVelocity;
+
+  private static GFTMovement movement;
+
+  public GunV4_GFSurfer_Merge_Shortend() {
+    movement = new GFTMovement(this);
+  }
+
 
   public void run() {
 
@@ -17,13 +30,9 @@ public class GunV4_GFSurfer_Merge extends AdvancedRobot {
     }
   }
 
-  public void onScannedRobot(ScannedRobotEvent e) {
-    // Lock Radar on target
-    setTurnRadarRight(2.0 * Utils.normalRelativeAngleDegrees(getHeading() + e.getBearing() - getRadarHeading()));
+  private double getInitialGuess(ScannedRobotEvent e) {
 
-    double bulletPower = getBulletPower(e.getDistance());
-    //System.out.println("bulletPower: " + bulletPower);
-    var bulletSpeed = Rules.getBulletSpeed(bulletPower);
+    var bulletSpeed = Rules.getBulletSpeed(BULLET_POWER);
 
     // Target position in relative coordinates
     double absoluteBearing = getHeadingRadians() + e.getBearingRadians();
@@ -67,80 +76,132 @@ public class GunV4_GFSurfer_Merge extends AdvancedRobot {
 
     //System.out.println("gunHeading: " + getGunHeading() + ", targetHeading: " + targetHeading + ", delta: " + delta);
 
-//    double gunTurn = Utils.normalRelativeAngleDegrees(delta);
-//    setTurnGunRight(gunTurn);
+    return Utils.normalRelativeAngleDegrees(delta);
+  }
+
+  static int hitCalcTotal = 0;
+  static int hitGuessTotal = 0;
+  static int bulletsFiredTotal = 0;
+  Map<Integer, Boolean> bullets = new HashMap<Integer, Boolean>();
+  Map<String, EnemyStats> enemys = new HashMap<String, EnemyStats>();
+
+  public void onScannedRobot(ScannedRobotEvent e) {
+
+    // Lock Radar on target
+    //setTurnRadarRight(2.0 * Utils.normalRelativeAngleDegrees(getHeading() + e.getBearing() - getRadarHeading()));
 
 
     double enemyAbsoluteBearing = getHeadingRadians() + e.getBearingRadians();
     double enemyDistance = e.getDistance();
     double enemyVelocity = e.getVelocity();
     if (enemyVelocity != 0) {
-      lateralDirection = kin.GFTUtils.sign(enemyVelocity * Math.sin(e.getHeadingRadians() - enemyAbsoluteBearing));
+      lateralDirection = GFTUtils2.sign(enemyVelocity * Math.sin(e.getHeadingRadians() - enemyAbsoluteBearing));
     }
-    kin.GFTWave wave = new kin.GFTWave(this);
+    GFTWave2 wave = new GFTWave2(this);
     wave.gunLocation = new Point2D.Double(getX(), getY());
-    kin.GFTWave.targetLocation = kin.GFTUtils.project(wave.gunLocation, enemyAbsoluteBearing, enemyDistance);
+    GFTWave2.targetLocation = GFTUtils2.project(wave.gunLocation, enemyAbsoluteBearing, enemyDistance);
     wave.lateralDirection = lateralDirection;
-    wave.bulletPower = 1.9;
+    wave.bulletPower = BULLET_POWER;
     wave.setSegmentations(enemyDistance, enemyVelocity, lastEnemyVelocity);
     lastEnemyVelocity = enemyVelocity;
     wave.bearing = enemyAbsoluteBearing;
 
-    setTurnGunRightRadians(Utils.normalRelativeAngle(enemyAbsoluteBearing - getGunHeadingRadians() + wave.mostVisitedBearingOffset()));
-    setFire(1.9);
+    double gunAdjust;
+
+    var enemy = enemys.get(e.getName());
+    if (enemy == null) {
+      enemy = new EnemyStats(e.getName());
+      enemys.put(e.getName(), enemy);
+    }
+
+    boolean aimWithGuess = enemy.shouldFireWithGuess();
+    if (aimWithGuess) {
+      gunAdjust = Utils.normalRelativeAngle(enemyAbsoluteBearing - getGunHeadingRadians() + wave.mostVisitedBearingOffset());
+      setTurnGunRightRadians(gunAdjust);
+      //System.out.println("Bin enemyDistance: " + enemyDistance + " enemyVelocity: " + enemyVelocity + " lastEnemyVelocity: " + lastEnemyVelocity);
+    } else {
+      gunAdjust = getInitialGuess(e);
+      setTurnGunRight(gunAdjust);
+      //System.out.println("Init enemyDistance: " + enemyDistance + " enemyVelocity: " + enemyVelocity + " lastEnemyVelocity: " + lastEnemyVelocity);
+    }
+
+    Bullet bullet;
+    if (getGunHeat() == 0 && gunAdjust < Math.atan2(9, e.getDistance()) && (bullet = setFireBullet(BULLET_POWER)) != null) {
+      bullets.put(bullet.hashCode(), aimWithGuess);
+      bulletsFiredTotal++;
+      enemy.bulletsFired++;
+      if (getEnergy() >= BULLET_POWER) {
+        addCustomEvent(wave);
+      }
+    }
+
+    //System.out.println("Bullets fired: " + bulletsFiredTotal + " hit Calc: " + hitCalcTotal + " hit Guess: " + hitGuessTotal);
+
+    movement.onScannedRobot(e);
+    setTurnRadarRightRadians(Utils.normalRelativeAngle(enemyAbsoluteBearing - getRadarHeadingRadians()) * 2);
   }
-
-  private static double lateralDirection;
-  private static double lastEnemyVelocity;
-
-  private double getBulletPower(double distance) {
-    double myEnergy = getEnergy();
-    double maxBulletPower = 3.0;  // Maximaler Wert für Schaden
-    double minBulletPower = 0.1;  // Verhindert Energieverschwendung
-
-    //System.out.println("distance: " + distance + ", myEnergy: " + myEnergy + ", Optimal Firepower: " + lastEnemyEnergy / 4);
-
-    // Falls vorher getroffen, Firepower anpassen
-    if (lastEnemyEnergy != 0.0 && lastEnemyEnergy < 15) {
-      // https://robowiki.net/wiki/Selecting_Fire_Power
-      double firePower = Math.min(lastEnemyEnergy / 4, maxBulletPower);
-      return Math.max(firePower, minBulletPower);
-    }
-
-    if (myEnergy < 10) {
-      return 0.5;
-    }
-
-    // Dynamische Anpassung an Distanz
-    if (distance > 400) {
-      return 1.0;
-    } else if (distance > 200) {
-      return 2.0;
-    }
-
-    return maxBulletPower;
-  }
-
-
-  private String lastEnemyName = "";
-  private double lastEnemyEnergy = 0.0;
 
   public void onBulletHit(BulletHitEvent event) {
-    lastEnemyName = event.getName();
-    lastEnemyEnergy = event.getEnergy();
-  }
-
-  public void onRobotDeath(RobotDeathEvent event) {
-    if (event.getName().equals(lastEnemyName)) {
-      lastEnemyName = "";
-      lastEnemyEnergy = 0.0;
+    var bulletHitWithGuess = bullets.get(event.getBullet().hashCode());
+    var enemy = enemys.get(event.getName());
+    if (enemy != null) {
+      if (bulletHitWithGuess) {
+        System.out.println(enemy.toString() + " - hit with guess");
+        enemy.hitGuess++;
+        hitGuessTotal++;
+      } else {
+        System.out.println(enemy.toString() + " - hit with calc");
+        enemy.hitCalc++;
+        hitCalcTotal++;
+      }
     }
   }
 
 }
 
 
-class GFTWave extends Condition {
+class EnemyStats {
+  public int hitCalc = 0;
+  public int hitGuess = 0;
+  public int bulletsFired = 0;
+  public String name = "";
+
+  EnemyStats(String name) {
+    this.name = name;
+  }
+
+  public boolean shouldFireWithGuess() {
+    Random r = new Random();
+    var hitsTotal = hitCalc + hitGuess;
+
+    if (hitsTotal <= 5) {
+      return r.nextBoolean();
+    }
+
+    double ratio = getWeightedRatio();
+    boolean fireWithGuess = r.nextDouble() <= ratio;
+//    if (fireWithGuess) {
+//      System.out.println("Ratio: " + String.format( "%.2f", ratio ) + " Fire with guess at " + name);
+//    } else {
+//      System.out.println("Ratio: " + String.format( "%.2f", ratio ) + " Fire with calc at " + name);
+//    }
+    return fireWithGuess;
+  }
+
+  private double getWeightedRatio() {
+    var hitsTotal = hitCalc + hitGuess;
+    return (double) (hitGuess == 0 ? 1 : hitGuess) / hitsTotal;
+  }
+
+  public String toString() {
+    return "Name: " + name + " Bullets fired: " + bulletsFired
+            + " hit Calc: " + hitCalc + " hit Guess: " + hitGuess
+            + " ratio: " + String.format( "%.2f", getWeightedRatio() );
+  }
+}
+
+
+class GFTWave2 extends Condition {
   // Static property holding the target location for the wave (where the bullet is aimed at)
   static Point2D targetLocation;
 
@@ -162,7 +223,7 @@ class GFTWave extends Condition {
   private static final int VELOCITY_INDEXES = 5;   // Number of bins for velocity categorization
   private static final int BINS = 25;              // Number of bins to track the opponent's escape angle
   private static final int MIDDLE_BIN = (BINS - 1) / 2; // Middle bin index used for normalization
-  private static final double MAX_ESCAPE_ANGLE = 0.7; // Maximum angle for predicting escape
+  private static final double MAX_ESCAPE_ANGLE = 0.2; // Maximum angle for predicting escape
   private static final double BIN_WIDTH = MAX_ESCAPE_ANGLE / (double)MIDDLE_BIN; // Width of each bin
 
   // A 4-dimensional array to store statistical data based on distance, velocity, and escape angle
@@ -178,17 +239,23 @@ class GFTWave extends Condition {
   private double distanceTraveled;
 
   // Constructor for initializing the GFTWave with the robot instance
-  GFTWave(AdvancedRobot _robot) {
+  GFTWave2(AdvancedRobot _robot) {
     this.robot = _robot;
   }
 
   // Method to test if the wave has arrived at its target and update the statistics
   public boolean test() {
-    System.out.println("test()");
     advance();  // Move the wave forward by a small increment
     if (hasArrived()) {  // Check if the wave has arrived at the target
       buffer[currentBin()]++;  // Update the statistics for the current "bin" (escape angle)
       robot.removeCustomEvent(this);  // Remove the custom event once the wave has arrived
+    }
+    return false;
+  }
+
+  public boolean hasBufferData() {
+    for (int i = 0; i < BINS; i++) {
+      if(buffer[i] > 0) return true;
     }
     return false;
   }
@@ -241,7 +308,7 @@ class GFTWave extends Condition {
 }
 
 
-class GFTUtils {
+class GFTUtils2 {
 
   // Method to calculate the bullet velocity based on the power
   static double bulletVelocity(double power) {
@@ -273,3 +340,44 @@ class GFTUtils {
   }
 }
 
+
+class GFTMovement {
+  private static final double BATTLE_FIELD_WIDTH = 800;
+  private static final double BATTLE_FIELD_HEIGHT = 600;
+  private static final double WALL_MARGIN = 18;
+  private static final double MAX_TRIES = 125;
+  private static final double REVERSE_TUNER = 0.421075;
+  private static final double DEFAULT_EVASION = 1.2;
+  private static final double WALL_BOUNCE_TUNER = 0.699484;
+
+  private AdvancedRobot robot;
+  private Rectangle2D fieldRectangle = new Rectangle2D.Double(WALL_MARGIN, WALL_MARGIN,
+          BATTLE_FIELD_WIDTH - WALL_MARGIN * 2, BATTLE_FIELD_HEIGHT - WALL_MARGIN * 2);
+  private double enemyFirePower = 3;
+  private double direction = 0.4;
+
+  GFTMovement(AdvancedRobot _robot) {
+    this.robot = _robot;
+  }
+
+  public void onScannedRobot(ScannedRobotEvent e) {
+    double enemyAbsoluteBearing = robot.getHeadingRadians() + e.getBearingRadians();
+    double enemyDistance = e.getDistance();
+    Point2D robotLocation = new Point2D.Double(robot.getX(), robot.getY());
+    Point2D enemyLocation = GFTUtils2.project(robotLocation, enemyAbsoluteBearing, enemyDistance);
+    Point2D robotDestination;
+    double tries = 0;
+    while (!fieldRectangle.contains(robotDestination = GFTUtils2.project(enemyLocation, enemyAbsoluteBearing + Math.PI + direction,
+            enemyDistance * (DEFAULT_EVASION - tries / 100.0))) && tries < MAX_TRIES) {
+      tries++;
+    }
+    if ((Math.random() < (GFTUtils2.bulletVelocity(enemyFirePower) / REVERSE_TUNER) / enemyDistance ||
+            tries > (enemyDistance / GFTUtils2.bulletVelocity(enemyFirePower) / WALL_BOUNCE_TUNER))) {
+      direction = -direction;
+    }
+    // Jamougha's cool way
+    double angle = GFTUtils2.absoluteBearing(robotLocation, robotDestination) - robot.getHeadingRadians();
+    robot.setAhead(Math.cos(angle) * 100);
+    robot.setTurnRightRadians(Math.tan(angle));
+  }
+}
